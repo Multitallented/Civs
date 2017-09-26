d33e08c8cc7c693d0b4d612c81d74651b198fff1
Dan Miller <dan.miller@verys.com>
2017-09-26 10:27:29 -0700

    added protections for interacting with blocks

--- src/org/redcastlemedia/multitallented/civs/protections/ProtectionHandler.java
+++ src/org/redcastlemedia/multitallented/civs/protections/ProtectionHandler.java
@@ -1,11 +1,13 @@
 package org.redcastlemedia.multitallented.civs.protections;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.redcastlemedia.multitallented.civs.regions.Region;
 import org.redcastlemedia.multitallented.civs.regions.RegionManager;
 import org.redcastlemedia.multitallented.civs.regions.RegionType;
@@ -28,6 +30,36 @@ public class ProtectionHandler implements Listener {
         }
 
         event.setCancelled(checkLocation(event.getBlockPlaced(), event.getPlayer(), "block_place"));
+    }
+
+    @EventHandler
+    public void onBlockInteract(PlayerInteractEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+        Material mat = event.getClickedBlock().getType();
+        if (mat == Material.WOODEN_DOOR ||
+                mat == Material.TRAP_DOOR ||
+                mat == Material.IRON_DOOR_BLOCK ||
+                mat == Material.IRON_TRAPDOOR) {
+            event.setCancelled(checkLocation(event.getClickedBlock(), event.getPlayer(), "door_use"));
+        } else if (mat == Material.CHEST ||
+                mat == Material.FURNACE ||
+                mat == Material.BURNING_FURNACE ||
+                mat == Material.TRAPPED_CHEST ||
+                mat == Material.ENDER_CHEST) {
+            event.setCancelled(checkLocation(event.getClickedBlock(), event.getPlayer(), "chest_use"));
+        } else if (mat == Material.CROPS ||
+                mat == Material.CARROT ||
+                mat == Material.POTATO) {
+            event.setCancelled(checkLocation(event.getClickedBlock(), event.getPlayer(), "block_break"));
+        } else if (mat == Material.LEVER ||
+                mat == Material.STONE_BUTTON ||
+                mat == Material.WOOD_BUTTON) {
+            event.setCancelled(checkLocation(event.getClickedBlock(), event.getPlayer(), "button_use"));
+        }
+        //TODO protect paintings
+        event.setCancelled(checkLocation(event.getClickedBlock(), event.getPlayer(), "block_use"));
     }
 
     private boolean checkLocation(Block block, Player player, String type) {
