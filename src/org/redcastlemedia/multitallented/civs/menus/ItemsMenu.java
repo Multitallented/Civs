3f5202bee9a1452f624acc223cefc68d55635a5c
Dan Miller <dan.miller@verys.com>
2017-10-06 08:45:35 -0700

    fixed a bug where uuid wasn't being applied properly to the civItem

--- src/org/redcastlemedia/multitallented/civs/menus/ItemsMenu.java
+++ src/org/redcastlemedia/multitallented/civs/menus/ItemsMenu.java
@@ -6,6 +6,9 @@ import org.bukkit.inventory.Inventory;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
 import org.redcastlemedia.multitallented.civs.util.CVItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemsMenu extends Menu {
     private static final String MENU_NAME = "CivsItems";
     public ItemsMenu() {
@@ -22,6 +25,10 @@ public class ItemsMenu extends Menu {
 
         int i=0;
         for (CVItem cvItem : civilian.getStashItems()) {
+            List<String> lore = new ArrayList<>();
+            lore.add(civilian.getUuid().toString());
+            lore.addAll(cvItem.getLore());
+            cvItem.setLore(lore);
             inventory.setItem(i, cvItem.createItemStack());
             i++;
         }
