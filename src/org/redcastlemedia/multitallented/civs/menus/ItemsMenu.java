cacf07edf5ee4464f1cb23dd54a9db3d3d60247e
Dan Miller <dan.miller@verys.com>
2017-10-06 13:40:43 -0700

    added description to the item menu

--- src/org/redcastlemedia/multitallented/civs/menus/ItemsMenu.java
+++ src/org/redcastlemedia/multitallented/civs/menus/ItemsMenu.java
@@ -4,7 +4,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.items.CivItem;
 import org.redcastlemedia.multitallented.civs.util.CVItem;
+import org.redcastlemedia.multitallented.civs.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +26,11 @@ public class ItemsMenu extends Menu {
         Inventory inventory = Bukkit.createInventory(null, getInventorySize(civilian.getStashItems().size()), MENU_NAME);
 
         int i=0;
-        for (CVItem cvItem : civilian.getStashItems()) {
+        for (CivItem cvItem : civilian.getStashItems()) {
             List<String> lore = new ArrayList<>();
             lore.add(civilian.getUuid().toString());
-            lore.addAll(cvItem.getLore());
+            lore.addAll(Util.parseColors(cvItem.getDescription()));
+//            lore.addAll(cvItem.getLore());
             cvItem.setLore(lore);
             inventory.setItem(i, cvItem.createItemStack());
             i++;
