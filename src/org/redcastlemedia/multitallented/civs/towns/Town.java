e2fd357a7018cb72c8c87b0d70206f2d9520cda3
Dan Miller <dan.miller@verys.com>
2017-12-01 14:49:28 -0800

    display town power

--- src/org/redcastlemedia/multitallented/civs/towns/Town.java
+++ src/org/redcastlemedia/multitallented/civs/towns/Town.java
@@ -7,15 +7,21 @@ import java.util.UUID;
 
 public class Town {
     private final String type;
+
+
+    private int maxPower;
+    private int power;
     private Location location;
     private String name;
     private HashMap<UUID, String> people;
 
-    public Town(String name, String type, Location location, HashMap<UUID, String> people) {
+    public Town(String name, String type, Location location, HashMap<UUID, String> people, int power, int maxPower) {
         this.name = name;
         this.type = type;
         this.location = location;
         this.people = people;
+        this.power = power;
+        this.maxPower = maxPower;
     }
 
     public String getType() {
@@ -26,6 +32,18 @@ public class Town {
     }
     public String getName() { return name; }
     public HashMap<UUID, String> getPeople() { return people; }
+    public int getMaxPower() {
+        return maxPower;
+    }
+    public int getPower() {
+        return power;
+    }
+    public void setMaxPower(int maxPower) {
+        this.maxPower = maxPower;
+    }
+    public void setPower(int power) {
+        this.power = power;
+    }
 
     public int countPeople(String role) {
         if (role == null) {
