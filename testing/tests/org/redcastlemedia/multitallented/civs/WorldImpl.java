afa621097b8da580720bc7bb8af738355127a18f
Multitallented <multitallented@gmail.com>
2018-08-28 06:30:45 -0700

    updated to spigot 1.13.1

--- testing/tests/org/redcastlemedia/multitallented/civs/WorldImpl.java
+++ testing/tests/org/redcastlemedia/multitallented/civs/WorldImpl.java
@@ -703,6 +703,16 @@ public class WorldImpl implements World {
     }
 
     @Override
+    public <T> void spawnParticle(Particle particle, Location location, int i, double v, double v1, double v2, double v3, T t, boolean b) {
+
+    }
+
+    @Override
+    public <T> void spawnParticle(Particle particle, double v, double v1, double v2, int i, double v3, double v4, double v5, double v6, T t, boolean b) {
+
+    }
+
+    @Override
     public Spigot spigot() {
         return null;
     }
