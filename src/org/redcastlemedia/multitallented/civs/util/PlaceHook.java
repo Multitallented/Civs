fa28f37d11b0d5eee47fd49b0dd5ee783a61e4c2
Multitallented <multitallented@gmail.com>
2019-04-18 22:37:58 -0700

    changed to placeholder expansion

--- src/org/redcastlemedia/multitallented/civs/util/PlaceHook.java
+++ src/org/redcastlemedia/multitallented/civs/util/PlaceHook.java
@@ -1,21 +1,32 @@
 package org.redcastlemedia.multitallented.civs.util;
 
 
-import me.clip.placeholderapi.external.EZPlaceholderHook;
-import org.bukkit.entity.Player;
-import org.redcastlemedia.multitallented.civs.Civs;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
 import org.redcastlemedia.multitallented.civs.civilians.CivilianManager;
 import org.redcastlemedia.multitallented.civs.towns.Town;
 import org.redcastlemedia.multitallented.civs.towns.TownManager;
 
-public class PlaceHook extends EZPlaceholderHook {
-    public PlaceHook() {
-        super(Civs.getInstance(), "civtownname");
+public class PlaceHook extends PlaceholderExpansion {
+
+    @Override
+    public String getIdentifier() {
+        return "civs";
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, String params) {
+    public String getAuthor() {
+        return "Multitallented";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String identifier) {
         if (player == null) {
             return "";
         }
