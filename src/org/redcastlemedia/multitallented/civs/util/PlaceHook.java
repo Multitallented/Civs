cbd8de087a05a327fda14fdc71b6dcad0a42fb82
Multitallented <multitallented@gmail.com>
2019-04-18 22:57:44 -0700

    revert

--- src/org/redcastlemedia/multitallented/civs/util/PlaceHook.java
+++ src/org/redcastlemedia/multitallented/civs/util/PlaceHook.java
@@ -1,27 +1,37 @@
 package org.redcastlemedia.multitallented.civs.util;
 
 
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.PlaceholderHook;
-import org.bukkit.entity.Player;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
 import org.redcastlemedia.multitallented.civs.civilians.CivilianManager;
 import org.redcastlemedia.multitallented.civs.towns.Town;
 import org.redcastlemedia.multitallented.civs.towns.TownManager;
 
-public class PlaceHook extends PlaceholderHook {
-    public PlaceHook() {
-        PlaceholderAPI.registerPlaceholderHook("civs", this);
+public class PlaceHook extends PlaceholderExpansion {
+
+    @Override
+    public String getIdentifier() {
+        return "civs";
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, String params) {
-        return "LouisWasWrong";
-//        if (player == null) {
-//            return "";
-//        }
-//        Civilian civilian = CivilianManager.getInstance().getCivilian(player.getUniqueId());
-//        return getReplacement(civilian);
+    public String getAuthor() {
+        return "Multitallented";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String identifier) {
+        if (player == null) {
+            return "";
+        }
+        Civilian civilian = CivilianManager.getInstance().getCivilian(player.getUniqueId());
+        return getReplacement(civilian);
     }
 
     private String getReplacement(Civilian civilian) {
