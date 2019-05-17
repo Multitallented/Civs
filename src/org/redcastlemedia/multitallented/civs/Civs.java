113d658011c8386b77d84b9cc3a2a44a6d49f28d
Dan Miller <dmiller@accurate.com>
2019-05-17 16:24:46 -0700

    re-add intruder

--- src/org/redcastlemedia/multitallented/civs/Civs.java
+++ src/org/redcastlemedia/multitallented/civs/Civs.java
@@ -196,7 +196,7 @@ public class Civs extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new EvolveEffect(), this);
 //        Bukkit.getPluginManager().registerEvents(new AntiCampEffect(), this);
         Bukkit.getPluginManager().registerEvents(new SiegeEffect(), this);
-//        Bukkit.getPluginManager().registerEvents(new IntruderEffect(), this);
+        Bukkit.getPluginManager().registerEvents(new IntruderEffect(), this);
 //        Bukkit.getPluginManager().registerEvents(new TemporaryEffect(), this);
 //        Bukkit.getPluginManager().registerEvents(new SpawnEffect(), this);
         Bukkit.getPluginManager().registerEvents(new RepairEffect(), this);
