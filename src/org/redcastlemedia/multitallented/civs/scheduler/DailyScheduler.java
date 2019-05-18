ca7daff23ffed81efb77cd2e787d322ae10b0d07
Multitallented <multitallented@gmail.com>
2019-05-17 19:02:06 -0700

    added voting to the daily scheduler

--- src/org/redcastlemedia/multitallented/civs/scheduler/DailyScheduler.java
+++ src/org/redcastlemedia/multitallented/civs/scheduler/DailyScheduler.java
@@ -1,5 +1,6 @@
 package org.redcastlemedia.multitallented.civs.scheduler;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import org.redcastlemedia.multitallented.civs.items.ItemManager;
 import org.redcastlemedia.multitallented.civs.regions.Region;
 import org.redcastlemedia.multitallented.civs.regions.RegionManager;
 import org.redcastlemedia.multitallented.civs.regions.RegionType;
+import org.redcastlemedia.multitallented.civs.towns.GovernmentType;
 import org.redcastlemedia.multitallented.civs.towns.Town;
 import org.redcastlemedia.multitallented.civs.towns.TownManager;
 
@@ -25,6 +27,52 @@ public class DailyScheduler implements Runnable {
         }
 
         doTaxes();
+        doVotes();
+    }
+
+    private void doVotes() {
+        for (Town town : TownManager.getInstance().getTowns()) {
+            if (town.getVotes().isEmpty()) {
+                continue;
+            }
+            if (town.getGovernmentType() != GovernmentType.DEMOCRACY &&
+                    town.getGovernmentType() != GovernmentType.DEMOCRATIC_SOCIALISM &&
+                    town.getGovernmentType() != GovernmentType.COOPERATIVE &&
+                    town.getGovernmentType() != GovernmentType.CAPITALISM) {
+                continue;
+            }
+            HashMap<UUID, Integer> voteTally = new HashMap<>();
+            for (UUID uuid : town.getVotes().keySet()) {
+                for (UUID cUuid : town.getVotes().get(uuid).keySet()) {
+                    if (voteTally.containsKey(cUuid)) {
+                        voteTally.put(cUuid, voteTally.get(cUuid) + town.getVotes().get(uuid).get(cUuid));
+                    } else {
+                        voteTally.put(cUuid, town.getVotes().get(uuid).get(cUuid));
+                    }
+                }
+            }
+            UUID mostVotes = null;
+            int mostVoteCount = 0;
+            for (UUID uuid : voteTally.keySet()) {
+                if (mostVoteCount < voteTally.get(uuid)) {
+                    mostVotes = uuid;
+                    mostVoteCount = voteTally.get(uuid);
+                }
+            }
+            if (mostVotes != null && town.getRawPeople().containsKey(mostVotes) &&
+                    !town.getRawPeople().get(mostVotes).contains("owner")) {
+                HashSet<UUID> setMembers = new HashSet<>();
+                for (UUID uuid : town.getRawPeople().keySet()) {
+                    if (town.getRawPeople().get(uuid).contains("owner")) {
+                        setMembers.add(uuid);
+                    }
+                }
+                for (UUID uuid : setMembers) {
+                    town.setPeople(uuid, "member");
+                }
+                town.setPeople(mostVotes, "owner");
+            }
+        }
     }
 
     private void doTaxes() {
