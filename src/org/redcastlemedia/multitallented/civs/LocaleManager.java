9a63374db455ddf0d7616117d42e6be75d161917
Dan Miller <dmiller@accurate.com>
2019-11-20 11:29:03 -0800

    added fallbacks for translations

--- src/org/redcastlemedia/multitallented/civs/LocaleManager.java
+++ src/org/redcastlemedia/multitallented/civs/LocaleManager.java
@@ -1,15 +1,23 @@
 package org.redcastlemedia.multitallented.civs;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.redcastlemedia.multitallented.civs.util.FallbackConfigUtil;
 import org.redcastlemedia.multitallented.civs.util.Util;
 
 public class LocaleManager {
 
+    private static String TRANSLATION_FOLDER_NAME = "translations";
     private static LocaleManager localeManager;
     HashMap<String, HashMap<String, String>> languageMap = new HashMap<>();
 
@@ -52,28 +60,37 @@ public class LocaleManager {
     }
 
     private void loadAllConfigs() {
-        File translationFolder = new File(Civs.getInstance().getDataFolder(), "translations");
-        if (!translationFolder.exists()) {
-            if (!translationFolder.mkdir()) {
-                Civs.logger.severe("Unable to load any translations!");
-                return;
+        File translationFolder = new File(Civs.getInstance().getDataFolder(), TRANSLATION_FOLDER_NAME);
+        boolean translationFolderExists = translationFolder.exists();
+        String path = "/resources/" + ConfigManager.getInstance().getDefaultConfigSet() + "/" + TRANSLATION_FOLDER_NAME;
+        InputStream in = getClass().getResourceAsStream(path);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        List<String> fileNames = new ArrayList<>();
+        String resource;
+        try {
+            while ((resource = reader.readLine()) != null) {
+                fileNames.add(resource);
             }
+        } catch (IOException io) {
+            Civs.logger.severe("Unable to load any translations!");
+            return;
         }
-        for (File currentTranslationFile : translationFolder.listFiles()) {
-            FileConfiguration config = new YamlConfiguration();
-            try {
-                config.load(currentTranslationFile);
-                HashMap<String, String> currentLanguage = new HashMap<>();
-
-                for (String translationKey : config.getKeys(false)) {
-                    currentLanguage.put(translationKey, config.getString(translationKey));
-                }
-
-                String langKey = currentTranslationFile.getName().replace(".yml", "");
-                languageMap.put(langKey, currentLanguage);
-            } catch (Exception e) {
-                Civs.logger.severe("Unable to load " + currentTranslationFile.getName());
+        for (String fileName : fileNames) {
+            FileConfiguration config;
+            if (translationFolderExists) {
+                config = FallbackConfigUtil.getConfig(
+                        new File(translationFolder, fileName), TRANSLATION_FOLDER_NAME + "/" + fileName);
+            } else {
+                config = FallbackConfigUtil.getConfig(null, TRANSLATION_FOLDER_NAME + "/" + fileName);
             }
+            HashMap<String, String> currentLanguage = new HashMap<>();
+
+            for (String translationKey : config.getKeys(false)) {
+                currentLanguage.put(translationKey, config.getString(translationKey));
+            }
+
+            String langKey = fileName.replace(".yml", "");
+            languageMap.put(langKey, currentLanguage);
         }
     }
 
