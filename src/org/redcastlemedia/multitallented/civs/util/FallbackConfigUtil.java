f4bf6adf25d00ee306c05a29acd7f5c520653561
Multitallented <multitallented@gmail.com>
2019-11-20 19:07:33 -0800

    optimize imports

--- src/org/redcastlemedia/multitallented/civs/util/FallbackConfigUtil.java
+++ src/org/redcastlemedia/multitallented/civs/util/FallbackConfigUtil.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 public final class FallbackConfigUtil {
     private FallbackConfigUtil() {
