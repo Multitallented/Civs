067928af55644e35af4e1457ca19f839160345d2
Dan Miller <dmiller@accurate.com>
2019-12-03 11:15:01 -0800

    added a unit test for admins

--- src/main/java/org/redcastlemedia/multitallented/civs/items/ItemManager.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/items/ItemManager.java
@@ -488,8 +488,9 @@ public class ItemManager {
             }
         }
         checkList.clear();
-        boolean isCivAdmin = Civs.perm != null &&
-                Civs.perm.has(Bukkit.getPlayer(civilian.getUuid()), "civs.admin");
+        Player player = Bukkit.getPlayer(civilian.getUuid());
+        boolean isCivAdmin = player != null && (player.isOp() || (Civs.perm != null &&
+                Civs.perm.has(player, "civs.admin")));
         for (CivItem item : returnList) {
             if (!hasItemUnlocked(civilian, item) ||
                     (isShop && !item.getInShop() && !isCivAdmin)) {
--- src/test/java/org/redcastlemedia/multitallented/civs/menus/ShopMenuTest.java
+++ src/test/java/org/redcastlemedia/multitallented/civs/menus/ShopMenuTest.java
@@ -1,14 +1,20 @@
 package org.redcastlemedia.multitallented.civs.menus;
 
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.junit.Before;
 import org.junit.Test;
+import org.redcastlemedia.multitallented.civs.PlayerInventoryImpl;
 import org.redcastlemedia.multitallented.civs.TestUtil;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
 import org.redcastlemedia.multitallented.civs.civilians.CivilianManager;
@@ -42,12 +48,33 @@ public class ShopMenuTest extends TestUtil {
         params.put("parent", "shops");
         Map<String, Object> data = MenuManager.menus.get("shop").createData(civilian, params);
         List<CivItem> shopItems = (List<CivItem>) data.get("shopItems");
-        HashSet<CivItem> items = new HashSet<>();
+        HashSet<String> items = new HashSet<>();
         for (CivItem civItem : shopItems) {
-            if (items.contains(civItem)) {
+            if (items.contains(civItem.getProcessedName())) {
                 fail("Duplicate item found " + civItem.getProcessedName());
             }
-            items.add(civItem);
+            items.add(civItem.getProcessedName());
+        }
+    }
+
+    @Test @SuppressWarnings("unchecked")
+    public void shopMenuShopsShouldNotDuplicateItemsForAdmins() {
+        UUID uuid = new UUID(1, 9);
+        Player player = mock(Player.class);
+        when(player.isOp()).thenReturn(true);
+        when(player.getInventory()).thenReturn(new PlayerInventoryImpl());
+        when(Bukkit.getServer().getPlayer(uuid)).thenReturn(player);
+        Civilian civilian = CivilianManager.getInstance().getCivilian(uuid);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("parent", "utilities");
+        Map<String, Object> data = MenuManager.menus.get("shop").createData(civilian, params);
+        List<CivItem> shopItems = (List<CivItem>) data.get("shopItems");
+        HashSet<String> items = new HashSet<>();
+        for (CivItem civItem : shopItems) {
+            if (items.contains(civItem.getProcessedName())) {
+                fail("Duplicate item found " + civItem.getProcessedName());
+            }
+            items.add(civItem.getProcessedName());
         }
     }
 
