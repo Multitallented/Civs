814f12726ee237fcc00bc5c2efc20e8f35a991c0
Dan Miller <dmiller@accurate.com>
2020-01-20 21:30:06 -0800

    added stubs for the nation and nation list menu

---
+++ src/main/java/org/redcastlemedia/multitallented/civs/menus/nations/NationListMenu.java
@@ -0,0 +1,15 @@
+package org.redcastlemedia.multitallented.civs.menus.nations;
+
+import java.util.Map;
+
+import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.menus.CivsMenu;
+import org.redcastlemedia.multitallented.civs.menus.CustomMenu;
+
+@CivsMenu(name = "nation-list")
+public class NationListMenu extends CustomMenu {
+    @Override
+    public Map<String, Object> createData(Civilian civilian, Map<String, String> params) {
+        return null;
+    }
+}
---
+++ src/main/java/org/redcastlemedia/multitallented/civs/menus/nations/NationMenu.java
@@ -0,0 +1,19 @@
+package org.redcastlemedia.multitallented.civs.menus.nations;
+
+import java.util.Map;
+
+import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.menus.CivsMenu;
+import org.redcastlemedia.multitallented.civs.menus.CustomMenu;
+
+@CivsMenu(name = "nation") @SuppressWarnings("unused")
+public class NationMenu extends CustomMenu {
+
+    @Override
+    public Map<String, Object> createData(Civilian civilian, Map<String, String> params) {
+
+        return null;
+    }
+
+
+}
