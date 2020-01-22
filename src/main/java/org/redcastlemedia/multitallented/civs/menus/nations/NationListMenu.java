2bf17b8cbc20e6e94de347c7153f1ba322f13272
Dan Miller <dmiller@accurate.com>
2020-01-21 16:04:19 -0800

    finished the nation list

--- src/main/java/org/redcastlemedia/multitallented/civs/menus/nations/NationListMenu.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/menus/nations/NationListMenu.java
@@ -1,15 +1,60 @@
 package org.redcastlemedia.multitallented.civs.menus.nations;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.items.CVItem;
+import org.redcastlemedia.multitallented.civs.localization.LocaleManager;
 import org.redcastlemedia.multitallented.civs.menus.CivsMenu;
 import org.redcastlemedia.multitallented.civs.menus.CustomMenu;
+import org.redcastlemedia.multitallented.civs.menus.MenuIcon;
+import org.redcastlemedia.multitallented.civs.menus.MenuManager;
+import org.redcastlemedia.multitallented.civs.nations.Nation;
+import org.redcastlemedia.multitallented.civs.nations.NationManager;
+import org.redcastlemedia.multitallented.civs.towns.Government;
+import org.redcastlemedia.multitallented.civs.towns.GovernmentManager;
+import org.redcastlemedia.multitallented.civs.towns.Town;
+import org.redcastlemedia.multitallented.civs.util.Constants;
 
-@CivsMenu(name = "nation-list")
+@CivsMenu(name = "nation-list") @SuppressWarnings("unused")
 public class NationListMenu extends CustomMenu {
     @Override
     public Map<String, Object> createData(Civilian civilian, Map<String, String> params) {
-        return null;
+        HashMap<String, Object> data = new HashMap<>();
+        if (params.containsKey("page")) {
+            data.put("page", Integer.parseInt(params.get("page")));
+        } else {
+            data.put("page", 0);
+        }
+        List<Nation> nationList = new ArrayList<>(NationManager.getInstance().getAllNations());
+        data.put("nationList", nationList);
+
+        int maxPage = (int) Math.ceil((double) nationList.size() / (double) itemsPerPage.get("nations"));
+        maxPage = maxPage > 0 ? maxPage - 1 : 0;
+        data.put("maxPage", maxPage);
+        return data;
+    }
+
+    @Override @SuppressWarnings("unchecked")
+    protected ItemStack createItemStack(Civilian civilian, MenuIcon menuIcon, int count) {
+        if (menuIcon.getKey().equals("nations")) {
+            List<Nation> nationList = (List<Nation>) MenuManager.getData(civilian.getUuid(), "nationList");
+            int page = (int) MenuManager.getData(civilian.getUuid(), "page");
+            int startIndex = page * menuIcon.getIndex().size();
+            if (nationList.size() <= startIndex + count) {
+                return new ItemStack(Material.AIR);
+            }
+            Nation nation = nationList.get(startIndex + count);
+            ItemStack itemStack = nation.getIcon();
+            putActions(civilian, menuIcon, itemStack, count);
+            return itemStack;
+        }
+        return super.createItemStack(civilian, menuIcon, count);
     }
 }
