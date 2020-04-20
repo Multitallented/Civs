0906591b78dd39cd05b58f1d6a1456f64ca62f77
Dan Miller <dmiller@accurate.com>
2020-04-19 18:36:01 -0700

    more stubs

---
+++ src/main/java/org/redcastlemedia/multitallented/civs/menus/nations/ClaimMenu.java
@@ -0,0 +1,16 @@
+package org.redcastlemedia.multitallented.civs.menus.nations;
+
+import java.util.Map;
+
+import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.menus.CivsMenu;
+import org.redcastlemedia.multitallented.civs.menus.CustomMenu;
+
+@CivsMenu(name = "claim") @SuppressWarnings("unused")
+public class ClaimMenu extends CustomMenu {
+
+    @Override
+    public Map<String, Object> createData(Civilian civilian, Map<String, String> params) {
+        return null;
+    }
+}
