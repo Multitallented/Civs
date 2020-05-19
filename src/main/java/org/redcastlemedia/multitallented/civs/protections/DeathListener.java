d924c835e10bb9e830d201a99d6b3cd48be0a5e9
Multitallented <multitallented@gmail.com>
2020-05-18 23:10:17 -0700

    added armor as a skill

--- src/main/java/org/redcastlemedia/multitallented/civs/protections/DeathListener.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/protections/DeathListener.java
@@ -46,6 +46,8 @@ import org.redcastlemedia.multitallented.civs.util.Constants;
 import org.redcastlemedia.multitallented.civs.util.Util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @CivsSingleton()
@@ -129,6 +131,8 @@ public class DeathListener implements Listener {
         Player player = (Player) event.getEntity();
         Civilian civilian = CivilianManager.getInstance().getCivilian(player.getUniqueId());
 
+        checkArmorSkill(player);
+
         if (!civilian.isInCombat()) {
             boolean setCancelled = event.isCancelled() ||
                     ProtectionHandler.shouldBlockAction(player.getLocation(), "deny_damage");
@@ -201,6 +205,43 @@ public class DeathListener implements Listener {
             return;
         }
         civilian.setLastDamager(damager.getUniqueId());
+    }
+
+    private void checkArmorSkill(Player player) {
+        Civilian civilian = CivilianManager.getInstance().getCivilian(player.getUniqueId());
+        Set<Material> armors = new HashSet<>();
+        ItemStack helmet = player.getInventory().getHelmet();
+        ItemStack chestPlate = player.getInventory().getHelmet();
+        ItemStack leggings = player.getInventory().getHelmet();
+        ItemStack boots = player.getInventory().getHelmet();
+        if (helmet != null && RepairEffect.isHelmet(helmet.getType())) {
+            armors.add(helmet.getType());
+        }
+        if (chestPlate != null && RepairEffect.isChestplate(chestPlate.getType())) {
+            armors.add(chestPlate.getType());
+        }
+        if (leggings != null && RepairEffect.isLeggings(leggings.getType())) {
+            armors.add(leggings.getType());
+        }
+        if (boots != null && RepairEffect.isBoots(boots.getType())) {
+            armors.add(boots.getType());
+        }
+        for (Skill skill : civilian.getSkills().values()) {
+            if (skill.getType().equalsIgnoreCase(CivSkills.ARMOR.name())) {
+                double exp = 0;
+                for (Material mat : armors) {
+                    exp += skill.addAccomplishment(mat.name());
+                }
+
+                if (exp > 0) {
+                    String localSkillName = LocaleManager.getInstance().getTranslationWithPlaceholders(player,
+                            skill.getType() + LocaleConstants.SKILL_SUFFIX);
+                    player.sendMessage(Civs.getPrefix() + LocaleManager.getInstance().getTranslationWithPlaceholders(player,
+                            "exp-gained").replace("$1", "" + exp)
+                            .replace("$2", localSkillName));
+                }
+            }
+        }
     }
 
     @EventHandler
