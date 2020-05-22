629679c45de0c7319f7bed026a20149ddd9e2751
Multitallented <multitallented@gmail.com>
2020-05-22 09:29:03 -0700

    finished the skills menu

--- src/main/java/org/redcastlemedia/multitallented/civs/localization/LocaleConstants.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/localization/LocaleConstants.java
@@ -11,6 +11,7 @@ public final class LocaleConstants {
     public static final String PREVENT_CIVS_ITEM_SHARE = "prevent-civs-item-share";
     public static final String REGION_PROTECTED = "region-protected";
     public static final String PERMISSION_DENIED = "no-permission";
+    public static final String SKILL_DESC_SUFFIX = "-skill-desc";
 
     private LocaleConstants() {
 
