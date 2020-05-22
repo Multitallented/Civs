bc0798cb1717ea20256f20ebd61404867fe45904
Dan Miller <dmiller@accurate.com>
2020-05-22 14:37:14 -0700

    classes are stored inside the civilian object since they can only belong to one civilian

--- src/main/java/org/redcastlemedia/multitallented/civs/civclass/ClassManager.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/civclass/ClassManager.java
@@ -4,6 +4,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.redcastlemedia.multitallented.civs.Civs;
 import org.redcastlemedia.multitallented.civs.ConfigManager;
+import org.redcastlemedia.multitallented.civs.civilians.Civilian;
+import org.redcastlemedia.multitallented.civs.civilians.CivilianManager;
 import org.redcastlemedia.multitallented.civs.items.ItemManager;
 import org.redcastlemedia.multitallented.civs.items.CVItem;
 
@@ -13,7 +15,6 @@ import java.util.*;
 
 public class ClassManager {
     private static ClassManager classManager = null;
-    private HashMap<UUID, Set<CivClass>> civClasses = new HashMap<>();
 
     public ClassManager() {
         this.classManager = this;
@@ -33,13 +34,11 @@ public class ClassManager {
                     int id = classConfig.getInt("id");
                     UUID uuid = UUID.fromString(classConfig.getString("uuid"));
                     String className = classConfig.getString("type");
-                    if (!civClasses.containsKey(uuid)) {
-                        civClasses.put(uuid, new HashSet<CivClass>());
-                    }
                     int manaPerSecond = classConfig.getInt("mana-per-second", 1);
                     int maxMana = classConfig.getInt("max-mana", 100);
 
-                    civClasses.get(uuid).add(new CivClass(id, uuid, className, manaPerSecond, maxMana));
+                    Civilian civilian = CivilianManager.getInstance().getCivilian(uuid);
+                    civilian.getCivClasses().add(new CivClass(id, uuid, className, manaPerSecond, maxMana));
                 } catch (Exception ex) {
                     Civs.logger.severe("Unable to load " + file.getName());
                     ex.printStackTrace();
@@ -50,13 +49,6 @@ public class ClassManager {
             Civs.logger.severe("Unable to load class files");
             return;
         }
-    }
-    public void addClass(CivClass civClass) {
-        if (civClasses.get(civClass.getUuid()) == null) {
-            civClasses.put(civClass.getUuid(), new HashSet<CivClass>());
-        }
-        civClasses.get(civClass.getUuid()).add(civClass);
-        saveClass(civClass);
     }
     public void saveClass(CivClass civClass) {
         File classFolder = new File(Civs.dataLocation, "class-data");
@@ -77,7 +69,11 @@ public class ClassManager {
             config.set("id", civClass.getId());
             config.set("type", civClass.getType());
             config.set("uuid", civClass.getUuid());
-            //TODO save attributes here as needed
+            config.set("mana-per-second", civClass.getManaPerSecond());
+            config.set("max-mana", civClass.getMaxMana());
+            for (Map.Entry<Integer, String> entry : civClass.getSelectedSpells().entrySet()) {
+                config.set("spells." + entry.getKey(), entry.getValue());
+            }
 
             config.save(classFile);
         } catch (Exception e) {
@@ -99,31 +95,6 @@ public class ClassManager {
         return i;
     }
 
-    public CivClass getCivClass(UUID uuid, String type) {
-        if (civClasses.get(uuid) == null) {
-            return null; //TODO perhaps fix this to be a default class?
-        }
-        for (CivClass civClass : civClasses.get(uuid)) {
-            if (civClass.getType().equals(type)) {
-                return civClass;
-            }
-        }
-        return null;
-    }
-    public CivClass getCivClass(UUID uuid, int id) {
-        if (civClasses.get(uuid) == null) {
-            return null;
-        }
-        for (CivClass civClass : civClasses.get(uuid)) {
-            if (civClass.getId() == id) {
-                return civClass;
-            }
-        }
-        return null;
-    }
-    public CivClass createClass(UUID uuid, String type, int manaPerSecond, int maxMana) {
-        return new CivClass(getNextId(), uuid, type, manaPerSecond, maxMana);
-    }
     public CivClass createDefaultClass(UUID uuid) {
         String className = ConfigManager.getInstance().getDefaultClass();
         ClassType classType = (ClassType) ItemManager.getInstance().getItemType(className);
