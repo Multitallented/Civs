6d1ec5c956000b128e7f63021a4d403942a91573
Multitallented <multitallented@gmail.com>
2020-05-23 10:21:22 -0700

    added an exp bar and fixed some skill menu displays

--- src/test/java/org/redcastlemedia/multitallented/civs/SkillTests.java
+++ src/test/java/org/redcastlemedia/multitallented/civs/SkillTests.java
@@ -3,10 +3,6 @@ package org.redcastlemedia.multitallented.civs;
 import static org.junit.Assert.assertEquals;
 
 import org.bukkit.Material;
-import org.bukkit.block.Biome;
-import org.bukkit.entity.EntityType;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.junit.Before;
 import org.junit.Test;
 import org.redcastlemedia.multitallented.civs.civilians.Civilian;
@@ -37,7 +33,7 @@ public class SkillTests extends TestUtil {
     public void expShouldCalcProperly() {
         skill.getAccomplishments().put(Material.OAK_PLANKS.name(), 9);
         skill.getAccomplishments().put(Material.CHEST.name(), 2);
-        assertEquals(48, skill.getExp(), 0.1);
+        assertEquals(48, skill.getTotalExp(), 0.1);
     }
 
     @Test
@@ -47,8 +43,29 @@ public class SkillTests extends TestUtil {
         skill.getAccomplishments().put(Material.CHEST.name(), 9);
         skill.getAccomplishments().put(Material.STICK.name(), 9);
         double expectedDiscount = (1.0 - (90.0 / 3000.0 * 0.3)) * regionType.getPrice();
-        assertEquals(90.0, skill.getExp(), 0.1);
+        assertEquals(90.0, skill.getTotalExp(), 0.1);
         assertEquals(expectedDiscount, SkillManager.getInstance().getSkillDiscountedPrice(civilian, regionType), 0.001);
+    }
+
+    @Test
+    public void skillBarShouldBeCorrectLength() {
+        skill.getAccomplishments().put(Material.OAK_PLANKS.name(), 9);
+        skill.getAccomplishments().put(Material.CHEST.name(), 9);
+        skill.getAccomplishments().put(Material.BREAD.name(), 9);
+        skill.getAccomplishments().put(Material.FURNACE.name(), 9);
+        skill.getAccomplishments().put(Material.STONE_PICKAXE.name(), 9);
+        skill.getAccomplishments().put(Material.OAK_PLANKS.name(), 9);
+        skill.getAccomplishments().put(Material.STONE_AXE.name(), 9);
+        skill.getAccomplishments().put(Material.OAK_BOAT.name(), 9);
+        skill.getAccomplishments().put(Material.CRAFTING_TABLE.name(), 9);
+        skill.getAccomplishments().put(Material.RED_BED.name(), 9);
+        skill.getAccomplishments().put(Material.STICK.name(), 9);
+        System.out.println(skill.getTotalExp());
+        System.out.println(skill.getLevel());
+        System.out.println(skill.getCurrentLevelExp());
+        System.out.println(skill.getExpToNextLevel());
+        System.out.println(skill.getCurrentExpAsBar("en"));
+        System.out.println(skill.getExpToNextLevelAsBar("en"));
     }
 
     @Test
