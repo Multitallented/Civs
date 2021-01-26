4131cbd54674747e03f24dba5c812829b7b4e8d3
NeumimTo <NeumimTo@users.noreply.github.com>
2021-01-26 01:30:59 +0100

    some fixes

--- src/main/java/org/redcastlemedia/multitallented/civs/commands/CreateNationCommand.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/commands/CreateNationCommand.java
@@ -9,6 +9,7 @@ import org.redcastlemedia.multitallented.civs.localization.LocaleManager;
 import org.redcastlemedia.multitallented.civs.nations.NationManager;
 import org.redcastlemedia.multitallented.civs.towns.Town;
 import org.redcastlemedia.multitallented.civs.towns.TownManager;
+import org.redcastlemedia.multitallented.civs.util.Constants;
 import org.redcastlemedia.multitallented.civs.util.Util;
 
 @CivsCommand(keys = { "create-nation" }) @SuppressWarnings("unused")
@@ -23,23 +24,34 @@ public class CreateNationCommand extends CivCommand {
             commandSender.sendMessage("Only in game players can use this command");
             return true;
         }
-        handleCreateNation((Player) commandSender, args[1]);
+        handleCreateNation((Player) commandSender, args);
         return true;
     }
 
-    private void handleCreateNation(Player player, String townName) {
+    private void handleCreateNation(Player player, String[] args) {
+        String townName = args[1];
         Town town = TownManager.getInstance().getTown(townName);
         if (town == null) {
             player.sendMessage(Civs.getPrefix() + LocaleManager.getInstance().getTranslation(player,
                     LocaleConstants.INVALID_TARGET));
             return;
         }
-        if (!NationManager.getInstance().canCreateNation(town)) {
-            player.sendMessage(Civs.getPrefix() + LocaleManager.getInstance().getTranslation(player,
-                    LocaleConstants.PERMISSION_DENIED));
+
+        if (NationManager.getInstance().canCreateNation(town)) {
+            NationManager.getInstance().createNation(town);
             return;
         }
-        NationManager.getInstance().createNation(town);
+
+        if (args.length >= 3 &&
+                player.hasPermission(Constants.ADMIN_PERMISSION) &&
+                "-f".equalsIgnoreCase(args[2]) &&
+                NationManager.getInstance().getNationByTownName(town.getName()) == null) {
+            NationManager.getInstance().createNation(town);
+            return;
+        }
+
+        player.sendMessage(Civs.getPrefix() + LocaleManager.getInstance().getTranslation(player,
+                LocaleConstants.PERMISSION_DENIED));
     }
 
     @Override
