9033998f4f00bc694e5a08686665b81018a87a41
Multitallented <multitallented@gmail.com>
2021-01-28 17:09:06 -0800

    delay upkeep by a minute if no region standby

--- src/main/java/org/redcastlemedia/multitallented/civs/regions/RegionManager.java
+++ src/main/java/org/redcastlemedia/multitallented/civs/regions/RegionManager.java
@@ -687,6 +687,9 @@ public class RegionManager {
         }
 
         Region region = new Region(regionType.getProcessedName(), people, location, radii, regionType.getEffects(), 0);
+        if (!ConfigManager.getInstance().isRegionStandby()) {
+            region.lastTick = new Date().getTime() - Math.min(60000, regionType.getPeriod() * 1000);
+        }
         addRegion(region);
         StructureUtil.removeBoundingBox(civilian.getUuid());
         forceLoadRegionChunk(region);
